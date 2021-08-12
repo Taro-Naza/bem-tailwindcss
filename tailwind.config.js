c200ee71f816b24b3b1af7cd8688b605e4391095
Taro-Naza <37082547+Taro-Naza@users.noreply.github.com>
2021-08-12 06:05:21 +0100

    github push

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				roboto: ['roboto', 'sans-serif'],
+			},
+			colors: {
+				brand: '#ff8589',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
